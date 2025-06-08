fbb01bedffc5b5604e20311119866ba19392f30c
Azadeh Pedramnia <pedramnia9084@gmail.com>
2025-06-08 18:16:05 +0100

    invoice number increase independent of two first letter

--- hr-backend/utils/invoiceUtils.js
+++ hr-backend/utils/invoiceUtils.js
@@ -32,22 +32,23 @@ function generateRemitDate() {
   return now.toLocaleDateString("en-GB", { day: "2-digit", month: "long", year: "numeric" });
 }
 
-
 async function getNextInvoiceNo(country) {
-  const prefix = (country.trim().toUpperCase().slice(0,2) + 'O');
-  const searchPrefix = prefix + '-';
-  const result = await pool.query(
-    `SELECT invoice_no FROM invoice WHERE invoice_no LIKE $1 ORDER BY id DESC LIMIT 1`,
-    [`${searchPrefix}%`]
-  );
+  const prefix = getInvoicePrefix(country);
   let lastNumber = 508;
+
+  const result = await pool.query(
+    `SELECT invoice_no FROM invoice ORDER BY id DESC LIMIT 1`
+  );
+
   if (result.rows.length) {
     const lastInvoiceNo = result.rows[0].invoice_no;
     const numPart = parseInt(lastInvoiceNo.split('-')[1]);
     if (!isNaN(numPart)) lastNumber = numPart;
   }
-  return generateInvoiceNo(country, lastNumber);
+
+  return `${prefix}-${lastNumber + 1}`;
 }
+
 
 module.exports = { 
   generateInvoiceNo, 
